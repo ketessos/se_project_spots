394acf2d0a208f9d0fbf72c461136ee136a7b43e
ketelin passos <ketelincorpus@gmail.comgit config --global user.name Ketelin>
2025-02-05 06:08:41 -0700

    New javascript lines fr the modal,

--- scripts/index.js
+++ scripts/index.js
@@ -8,11 +8,18 @@ const initialCards = [
 ]
 
 const profileEditButton = document.querySelector(".profile__edit-btn");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
 
 const editModal = document.querySelector("#edit-modal");
+const editFormElement = editModal.querySelector (".modal__form");
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+const editModalNameInput = editModal.querySelector("#profile-name-imput");
+const editModalDescriptionInput = editModal.querySelector("#profile-description-input");
 
 function openModal () {
+    editModalNameInput.value = profileName.textContent
+    editModalDescriptionInput.value = profileDescription.textContent
     editModal.classList.add("modal_opened");
 }
 
@@ -20,5 +27,13 @@ function closeModal () {
     editModal.classList.remove("modal_opened");
 }
 
+function handleEditFormSubmit (evt){
+    evt.preventDefault();
+    profileName.textContent = editModalNameInput.value
+    profileDescription.textContent = editModalDescriptionInput.value
+    closeModal();
+}
+
 profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
